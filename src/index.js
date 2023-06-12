e970a14ca4772bbd70f5ed503d9ca8a2862d4498
MarcosAdrianoGuedes <marcosadri55@hotmail.com>
2023-06-12 00:32:50 -0300

    aplicativo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
